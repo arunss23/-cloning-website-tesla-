ff77cf4b1a6a7cea6f5b4520cd50faf05e346c4f
arunss23 <arunadevi3111@gmail.com>
2024-07-31 18:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.order-btn').addEventListener('click', function() {
+    alert('Order Now button clicked!');
+});
+
+document.querySelector('.demo-btn').addEventListener('click', function() {
+    alert('Demo Drive button clicked!');
+});
